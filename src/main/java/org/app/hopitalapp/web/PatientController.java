8f172001f98790c2e2f7a674649519f859a84f61
SoukainaLAKBICHI <soukaina.lakbichi-etu@etu.univh2c.ma>
2025-03-19 14:10:14 +0000

    Pagination

--- src/main/java/org/app/hopitalapp/web/PatientController.java
+++ src/main/java/org/app/hopitalapp/web/PatientController.java
@@ -3,9 +3,12 @@ package org.app.hopitalapp.web;
 import lombok.AllArgsConstructor;
 import org.app.hopitalapp.entities.Patient;
 import org.app.hopitalapp.repository.PatientRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -14,9 +17,12 @@ import java.util.List;
 public class PatientController {
     private PatientRepository patientRepository;
     @GetMapping("/index")
-    public String index(Model model) {
-        List<Patient> patientList = patientRepository.findAll();
-        model.addAttribute("ListPatients",patientList);
+    public String index(Model model, @RequestParam int page, @RequestParam int size) {
+        Page<Patient> patientPage = patientRepository.findAll(PageRequest.of(page, size));
+
+        List<Patient> patientList = patientPage.getContent();
+        model.addAttribute("ListPatients", patientList);
+
         return "patient";
     }
 }
